e62e9a874d8e055b633e5735973cc83acb1a428e
Mario76 <2001cupid@gmail.com>
2024-05-19 18:26:21 +0900

    Feat: File multi upload complete with only fix controller

--- src/main/java/com/khu/cloudcomputing/khuropbox/files/controller/FilesController.java
+++ src/main/java/com/khu/cloudcomputing/khuropbox/files/controller/FilesController.java
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 @Slf4j
 @RestController
@@ -43,31 +44,34 @@ public class FilesController {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
     @PostMapping("upload")
-    public ResponseEntity<?> Upload(@RequestPart(value="fileName") String fileName, @RequestPart(value = "file") MultipartFile multipartFile) {
+    public ResponseEntity<?> Upload(@RequestPart(value = "file") List<MultipartFile> multipartFiles) {
         String fileLink = "";
-        if (multipartFile != null) { // 파일 업로드한 경우에만
-            try {// 파일 업로드
-                Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
-                String id=authentication.getName();
-                FilesDTO file=new FilesDTO();
-                file.setFileName(fileName);
-                file.setFileSize(multipartFile.getSize());
-                file.setFileLink(fileLink);
-                file.setOwner(userRepository.findAllById(id).orElseThrow());
-                String fileType=multipartFile.getOriginalFilename().substring(multipartFile.getOriginalFilename().lastIndexOf(".")+1);
-                file.setFileType(fileType);
-                Integer index=filesService.insertFile(file);
-                fileLink = filesService.upload(multipartFile, id+'/', index, fileType); // S3 버킷의 images 디렉토리 안에 저장됨, S3에 저장된 이름은 id값으로 부여.
-                fileLink= URLDecoder.decode(fileLink, StandardCharsets.UTF_8);
-                log.info("fileLink = " + fileLink);
-                filesService.updateLink(index, fileLink);
-                return ResponseEntity.ok(filesService.findById(index));
-            } catch (IOException e) {
-                log.info("error");
-                return ResponseEntity.badRequest().build();
+        List<FilesDTO> filesList=new ArrayList<>();
+        for(MultipartFile multipartFile:multipartFiles) {
+            if (multipartFile != null) { // 파일 업로드한 경우에만
+                try {// 파일 업로드
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    String id = authentication.getName();
+                    FilesDTO file = new FilesDTO();
+                    file.setFileName(multipartFile.getOriginalFilename());
+                    file.setFileSize(multipartFile.getSize());
+                    file.setFileLink(fileLink);
+                    file.setOwner(userRepository.findAllById(id).orElseThrow());
+                    String fileType = multipartFile.getOriginalFilename().substring(multipartFile.getOriginalFilename().lastIndexOf(".") + 1);
+                    file.setFileType(fileType);
+                    Integer index = filesService.insertFile(file);
+                    fileLink = filesService.upload(multipartFile, id + '/', index, fileType); // S3 버킷의 images 디렉토리 안에 저장됨, S3에 저장된 이름은 id값으로 부여.
+                    fileLink = URLDecoder.decode(fileLink, StandardCharsets.UTF_8);
+                    log.info("fileLink = " + fileLink);
+                    filesService.updateLink(index, fileLink);
+                    filesList.add(filesService.findById(index));
+                } catch (IOException e) {
+                    log.info("error");
+                    return ResponseEntity.badRequest().build();
+                }
             }
         }
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(filesList);
     }
     @GetMapping("download/{fileId}")
     public ResponseEntity<?> Download(@PathVariable(value="fileId") Integer fileId) throws IOException {
